b1ceb73903ef44fb3ea60827718343dd4dd229a7
the-fool <sketchbang@gmail.com>
2016-03-26 15:19:44 -0500

    remove unneeded template features

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,27 +13,9 @@
 
         $urlRouterProvider.otherwise('/sample');
 
-        /**
-         * Layout Style Switcher
-         *
-         * This code is here for demonstration purposes.
-         * If you don't need to switch between the layout
-         * styles like in the demo, you can set one manually by
-         * typing the template urls into the `State definitions`
-         * area and remove this code
-         */
-        // Inject $cookies
-        var $cookies;
-
-        angular.injector(['ngCookies']).invoke([
-            '$cookies', function (_$cookies)
-            {
-                $cookies = _$cookies;
-            }
-        ]);
 
         // Get active layout
-        var layoutStyle = $cookies.get('layoutStyle') || 'verticalNavigation';
+        var layoutStyle = 'verticalNavigation';
 
         var layouts = {
             verticalNavigation  : {
